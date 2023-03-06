96dbf938d6789f9e26eb9e2ed1bec3efe76f5c09
Ermia Shabani <ermiashabanii@gmail.com>
2023-03-06 18:22:12 +0330

    added sleep time

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.keys import Keys
+from time import sleep
 
 options = webdriver.ChromeOptions()
 options.add_experimental_option("detach", True)
-driver = webdriver.Chrome(options=options, service= Service(ChromeDriverManager().install()))
+driver = webdriver.Chrome(options=options, service=Service(ChromeDriverManager().install()))
 
 driver.maximize_window()
 
@@ -13,8 +14,6 @@ driver.get("https://www.google.com")
 # a= "W0wltc"
 # const1 = driver.find_element('id', a)
 # const1.send_keys(Keys.ENTER)
-
-search = driver.find_element('name', 'q')
-search.send_keys("Selenium is fun! test 1")
-search.send_keys(Keys.ENTER)
-
+search = driver.find_element('name', 'q').send_keys("Selenium is fun! test 1" , Keys.ENTER)
+sleep(0.5)
+driver.quit()
