9abc45bab5e1dc6da38b492100aaac1e00da7b02
Ermia Shabani <ermiashabanii@gmail.com>
2023-03-07 14:59:16 +0330

    scroll and take screenshot

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.keys import Keys
 from time import sleep
+import os
+from pathlib import Path
 
 options = webdriver.ChromeOptions()
 options.add_experimental_option("detach", True)
@@ -10,7 +12,7 @@ driver = webdriver.Chrome(options=options, service=Service(ChromeDriverManager()
 
 driver.maximize_window()
 
-driver.get("https://www.google.com")
+# driver.get("https://www.google.com")
 # a= "W0wltc"
 # const1 = driver.find_element('id', a)
 # const1.send_keys(Keys.ENTER)
@@ -20,13 +22,24 @@ driver.get("https://www.google.com")
 # print(wt)
 # driver.get("https://www.digikala.com")
 # sleep(1)
-driver.refresh()
-driver.switch_to.new_window("window")
-driver.get("https://www.bing.com")
-sleep(2)
-b = driver.window_handles
-driver.switch_to.window(b[0])
-sleep(3)
+# driver.refresh()
+# driver.switch_to.new_window("window")
+driver.get("https://www.yahoo.com")
+sleep(1)
+# b = driver.window_handles
+# driver.switch_to.window(b[0])
+# driver.close()
+# driver.switch_to.window(b[1])
+# print(driver.get_window_size())
+#
+# sleep(1)
 # driver.back()
 # driver.forward()
+#scrool to bottem one time
+driver.execute_script("window/scrollTo(0, document.body.scrollHeight);")
+sleep(3)
+
+parent_path = Path(__file__).parent
+ax = os.path.join(str(parent_path),'ss.png')
+driver.save_screenshot(ax)
 driver.quit()
